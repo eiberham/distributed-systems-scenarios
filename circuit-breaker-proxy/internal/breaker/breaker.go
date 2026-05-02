58050b1e830a131819e294518add3742b85a3700
eiberham <cedenoabraham@gmail.com>
2026-05-02 17:59:50 -0300

    circuit breaker update

--- circuit-breaker-proxy/internal/breaker/breaker.go
+++ circuit-breaker-proxy/internal/breaker/breaker.go
@@ -1,6 +1,7 @@
 package breaker
 
 import (
+	"errors"
 	"sync"
 	"time"
 )
@@ -18,6 +19,8 @@ type CircuitBreaker struct {
 	name             string
 	failureThreshold int
 	state            State
+	failureCount     int
+	lastFailureTime  time.Time
 	resetTimeout     time.Duration
 }
 
@@ -27,5 +30,55 @@ func New(name string, failureThreshold int, state State, resetTimeout time.Durat
 		failureThreshold: failureThreshold,
 		state:            state,
 		resetTimeout:     resetTimeout,
+	}
+}
+
+func (cb *CircuitBreaker) Run(call func() (interface{}, error)) (interface{}, error) {
+	cb.Mutex.Lock()
+
+	if cb.state == StateOpen {
+		if time.Since(cb.lastFailureTime) > cb.resetTimeout {
+			cb.state = StateHalfOpen
+		} else {
+			cb.Mutex.Unlock()
+			return nil, errors.New("circuit is open")
+		}
+	}
+	cb.Mutex.Unlock()
+
+	response, err := call()
+
+	cb.Mutex.Lock()
+	defer cb.Mutex.Unlock()
+
+	if err != nil {
+		cb.recordFailure()
+		return nil, err
+	}
+
+	cb.recordSuccess()
+	return response, nil
+}
+
+func (cb *CircuitBreaker) recordSuccess() {
+	cb.failureCount = 0
+	if cb.state == StateHalfOpen {
+		cb.changeState(StateClosed)
+	}
+}
+
+func (cb *CircuitBreaker) recordFailure() {
+	cb.failureCount++
+
+	// If we're in half open and fail, or if we hit the threshold in closed
+	if cb.state == StateHalfOpen || cb.failureCount >= cb.failureThreshold {
+		cb.changeState(StateOpen)
+	}
+}
+
+func (cb *CircuitBreaker) changeState(newState State) {
+	cb.state = newState
+	if newState == StateOpen {
+		cb.lastFailureTime = time.Now()
 	}
 }
--- circuit-breaker-proxy/services/gateway/main.go
+++ circuit-breaker-proxy/services/gateway/main.go
@@ -1,15 +1,18 @@
 package main
 
 import (
-	cb "circuit-breaker-proxy/internal/breaker"
+	breaker "circuit-breaker-proxy/internal/breaker"
 	"circuit-breaker-proxy/internal/config"
+	"errors"
+	"io"
+	"net/http"
 	"time"
 
 	"github.com/labstack/echo/v5"
 )
 
 type Gateway struct {
-	breaker map[string]*cb.CircuitBreaker
+	breaker map[string]*breaker.CircuitBreaker
 }
 
 func main() {
@@ -23,8 +26,58 @@ func main() {
 		return c.String(200, "Working!")
 	})
 
-	cart := cb.New("cart", 5, cb.StateClosed, 10*time.Second)
-	products := cb.New("products", 5, cb.StateClosed, 10*time.Second)
+	cart := breaker.New("cart", 5, breaker.StateClosed, 10*time.Second)
+
+	prod := breaker.New("products", 5, breaker.StateClosed, 10*time.Second)
+
+	routes := e.Group("/api")
+
+	routes.GET("/products", func(c *echo.Context) error {
+
+		response, err := prod.Run(func() (interface{}, error) {
+			resp, e := http.Get("http://products:8083/products")
+			if e != nil {
+				return nil, e
+			}
+			defer resp.Body.Close()
+
+			if resp.StatusCode >= 500 {
+				return nil, errors.New("service error")
+			}
+
+			return io.ReadAll(resp.Body)
+		})
+
+		if err != nil {
+			// If the breaker is open, we return service unavailable
+			return c.JSON(http.StatusServiceUnavailable, map[string]string{"error": "Service temporarily down"})
+		}
+
+		return c.Blob(http.StatusOK, "application/json", response.([]byte))
+
+	})
+
+	routes.GET("/cart", func(c *echo.Context) error {
+		response, err := cart.Run(func() (interface{}, error) {
+			resp, e := http.Get("http://cart:8082/cart")
+			if e != nil {
+				return nil, e
+			}
+			defer resp.Body.Close()
+
+			if resp.StatusCode >= 500 {
+				return nil, errors.New("service error")
+			}
+
+			return io.ReadAll(resp.Body)
+		})
+
+		if err != nil {
+			return c.JSON(http.StatusServiceUnavailable, map[string]string{"error": "Service temporarily down"})
+		}
+
+		return c.Blob(http.StatusOK, "application/json", response.([]byte))
+	})
 
 	if err := e.Start(":" + config.Cfg.Port); err != nil {
 		e.Logger.Error("failed to start server", "error", err)
